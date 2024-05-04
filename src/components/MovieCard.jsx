f70b6512f0a43fb43de87e423633af709924079d
Santosh kumar gupta <sainkee1997@gmail.com>
2024-05-04 21:06:22 +0530

    rounded fixed

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -7,10 +7,10 @@ export default function MovieCard(props) {
   return (
     <>
       <div
-        className="h-[70vh] w-[80vw] bg-cover  text-white bg-center p-20 shadow-custom-shadow rounded-2xl  mx-auto relative "
+        className="h-[70vh] w-[80vw] bg-cover  text-white bg-center p-20 shadow-custom-shadow rounded-md  mx-auto relative "
         style={{ backgroundImage: `url(${props.image2})` }}
       >
-        <div className=" absolute top-0 left-0 right-0 bottom-0 z-100 bg-gradient-to-r from-black/100 via-black/80 to-black/10     "></div>
+        <div className=" absolute top-0 left-0 right-0 bottom-0 z-100 bg-gradient-to-r from-black/100 via-black/80 to-black/10  rounded-md   "></div>
         <div className="flex absolute top-0 left-0 bottom-0 right-0 p-3 md:p-8 ">
           <div className="flex flex-col w-full gap-5  justify-center md:gap-10 ">
             <div className="h-[40%] gap-x-5 md:gap-x-10  w-full flex">
